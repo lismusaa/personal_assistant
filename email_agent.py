bbdf7628d767960b3d0d93bbbb8784271b9d299b
lismusa-pp <lis.musa13@gmail.com>
2025-10-03 00:44:11 +0200

    Create email_agent.py
    Email agent placeholder.

---
+++ email_agent.py
@@ -0,0 +1,12 @@
+def fetch_unread_emails():
+    """
+    TODO: implement Gmail API fetch
+    """
+    return []
+
+
+def send_email(to_address: str, subject: str, body: str):
+    """
+    TODO: implement Gmail API or SMTP send
+    """
+    raise NotImplementedError("Email sending not implemented yet.")
